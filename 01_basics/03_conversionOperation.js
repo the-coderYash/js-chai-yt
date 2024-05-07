6b1430e6f0c088b2476e638872cd6d2610cd08c9
Yash Raj <yashraj00112@gmail.com>
2024-05-07 21:56:26 +0530

    //Changing data types

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,4 +1,14 @@
-let score = "56"
+let score = "goku"
 
 console.log(typeof score);
 console.log(typeof (score));
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+//If we covert a number like "56" it gets easily converted into a number => 56
+
+//But if we convert a value like "56xyz" it's not getting converted and it gives a value => Nan(Means - Not any Number) but the type of Nan is Number (It's a little bit confusing)
