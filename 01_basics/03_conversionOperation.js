d337d138a244e94cabd5a2abf090258d535bf39f
Yash Raj <yashraj00112@gmail.com>
2024-05-07 22:04:20 +0530

    //Discussed about Conversion Operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -12,3 +12,18 @@ console.log(valueInNumber);
 //If we covert a number like "56" it gets easily converted into a number => 56
 
 //But if we convert a value like "56xyz" it's not getting converted and it gives a value => Nan(Means - Not any Number) but the type of Nan is Number (It's a little bit confusing)
+
+//if we convert - 
+// true => 1;
+// false => 0;
+
+let isLoggedIn = "yash"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+
+// 1 => true
+// 0 => false
+//"" => false
+// "yash" => true
